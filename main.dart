ee927756931d6c7ffc0cdb4713184e4cc43d766f
Justin Varghese <justinv2006@gmail.com>
2020-11-01 07:12:20 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,16 @@
+// dart:io
+
+import "dart:io";
+
+main() {
+var firstName = "Justin";
+String lastName = "Varghese";
+
+print(firstName+ ' ' + lastName);
+}
+
+justin() {
+ stdout.writeIn('what is your name: ?');
+ String name = stdin.readLineSync();
+ print("My name is $name");
+}
